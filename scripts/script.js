2c11549759f0bd82ab1d4ed0c34a6b5b35d95e0d
jpedross1 <jpedross@sapo.pt>
2023-01-18 16:35:04 +0000

    random shit

--- scripts/script.js
+++ scripts/script.js
@@ -15,3 +15,8 @@ function showSlides() {
   setTimeout(showSlides, 5000); // Muda de imagem a cada 5s
 }
 
+function animateContacts(){
+  document.getElementById("contactAnimation").style.display = "block";
+  document.getElementById("contactAnimation").style.animation = "showContacts 2s ease-in-out";
+  setTimeout(function(){document.getElementById("contactAnimation").style.display = "none"}, 5000);
+}
